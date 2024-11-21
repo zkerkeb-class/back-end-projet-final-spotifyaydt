a570a1ce9d801c4ca5efd6b5a1818b900840170e
AlexismlaWeb <102291173+AlexismlaWeb@users.noreply.github.com>
2024-11-21 15:50:20 +0100

    Feat: Inialisation a la base de données
    Co-Authored-By: NegYanis16 <122283603+NegYanis16@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// server.js
+import express, { json } from 'express';
+import connectDB from './src/config/db.js'; // Fichier qui contient la logique de connexion
 
+const app = express();
+
+app.use(json());
+
+// Connexion à la base de données
+connectDB();
+
+// Exemple de route
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello, this is the API!');
 });
 
+// Démarrer le serveur
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
