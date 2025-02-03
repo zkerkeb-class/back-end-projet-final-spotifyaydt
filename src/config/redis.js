fce3d411c0f9057c3636f177a3d9538aaabbbba1
AlexismlaWeb <102291173+AlexismlaWeb@users.noreply.github.com>
2025-02-03 21:09:36 +0100

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,17 +2,17 @@ import { createClient } from '@redis/client';
 import logger from './logger.js';
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379', // Utilise REDIS_URL de Heroku
+  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
   socket: {
-    tls: true, // Activer le SSL/TLS
-    rejectUnauthorized: false, // Accepter les certificats auto-signés
+    tls: true, // Utilisation du protocole SSL/TLS
+    rejectUnauthorized: false, // Désactiver la vérification des certificats auto-signés
   },
 });
 
 redisClient
   .connect()
   .then(() => {
-    logger.info(`Connected to Redis at ${process.env.REDIS_URL}`);
+    logger.info('Connected to Redis');
   })
   .catch((err) => {
     logger.error('Redis connection error:', err);
