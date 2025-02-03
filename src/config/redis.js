6f6edf2419372cb52ac32a25333ec47bcf194e44
AlexismlaWeb <102291173+AlexismlaWeb@users.noreply.github.com>
2025-02-03 21:14:14 +0100

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,13 @@
 import { createClient } from '@redis/client';
 import logger from './logger.js';
 
+const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
 const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+  url: redisUrl,
   socket: {
-    tls: true, // Utilisation du protocole SSL/TLS
-    rejectUnauthorized: false, // Désactiver la vérification des certificats auto-signés
+    tls: true, // Utilisation du protocole SSL/TLS pour la connexion sécurisée
+    rejectUnauthorized: false, // Désactive la vérification des certificats auto-signés
   },
 });
 
